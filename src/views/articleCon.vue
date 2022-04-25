1017712d316abea65cfd46d1598c23649ab506e9
Occur55170 <42513736+Occur55170@users.noreply.github.com>
2022-04-25 23:40:42 +0800

    初步串聯成功

--- src/views/articleCon.vue
+++ src/views/articleCon.vue
@@ -15,7 +15,7 @@
     </div>
     <div class="message">
       <p>共 {{ messageList.length }} 則留言</p>
-      <ul>
+      <!-- <ul>
         <li v-for="(item,key) in messageList" :key="key">
           <div class="mainReply">
             <div class="avatar">
@@ -42,7 +42,7 @@
             </div>
           </div>
         </li>
-      </ul>
+      </ul> -->
       <div class="newMessage">
         <input type="text" class="form-control" id="name" name="姓名" v-model.trim="newMessage.author" placeholder="請輸入姓名" required maxlength="20">
         <div>
@@ -51,7 +51,7 @@
         </div>
         <textarea name="" id="" cols="30" rows="10" v-model.trim="newMessage.content"></textarea>
         <!-- <button class="btn btn-danger" type="submit" @click="addMessage" :disabled="submit">送出</button> -->
-        <!-- <button class="btn btn-danger" style="margin-right: 10px;" type="submit" @click="getMessage">get</button> -->
+        <button class="btn btn-danger" style="margin-right: 10px;" type="submit" @click="getMessage">get</button>
         <button class="btn btn-danger" type="submit" @click="addMessage">add</button>
       </div>
     </div>
@@ -59,7 +59,7 @@
 </template>
 
 <script>
-// import $ from 'jquery'
+import $ from 'jquery'
 // import axios from 'axios'
 
 export default {
@@ -75,7 +75,8 @@ export default {
         content: '',
         time: '',
         good: ''
-      }
+      },
+      RWW: ''
     }
   },
   methods: {
@@ -93,65 +94,98 @@ export default {
     goback () {
       this.$router.push('/')
     },
+    // getMessage () {
+    //   const vm = this
+    //   vm.$emit('load', true)
+    //   let mid = JSON.stringify({ 'mid': vm.Aid })
+    //   // msgapi
+    //   let api = `https://script.google.com/macros/s/AKfycbySNI7cijbkYD9Wzw3Qb22XUHwHkRj8c7utSYAA6NAmGADK-wL1nCAyoo6VA_cfBQD4/exec`
+    //   vm.$http.post(api, mid).then(response => {
+    //     console.log(response.data.data)
+    //     vm.messageList = response.data.data.map(item => {
+    //       item.mainMsg = JSON.parse(item.mainMsg)
+    //       if (item.reply.length !== 0) {
+    //         item.reply = item.reply.map(element => {
+    //           element = JSON.parse(element)
+    //           return element
+    //         })
+    //       }
+    //       return item
+    //     })
+    //     vm.$emit('load', false)
+    //   })
+    // },
     getMessage () {
       const vm = this
-      vm.$emit('load', true)
       let mid = JSON.stringify({ 'mid': vm.Aid })
       // msgapi
-      let api = `https://script.google.com/macros/s/AKfycbwrLoDh54vq48S5kbwCXSsx3Qq0ev4yH1Q3LH65yCsRoqSRojqqlqxHgGNTN-vOR5tb/exec`
+      let api = `https://script.google.com/macros/s/AKfycbySNI7cijbkYD9Wzw3Qb22XUHwHkRj8c7utSYAA6NAmGADK-wL1nCAyoo6VA_cfBQD4/exec`
       vm.$http.post(api, mid).then(response => {
-        console.log(response.data.data)
-        vm.messageList = response.data.data.map(item => {
-          item.mainMsg = JSON.parse(item.mainMsg)
-          if (item.reply.length !== 0) {
-            item.reply = item.reply.map(element => {
-              element = JSON.parse(element)
-              return element
-            })
-          }
-          return item
+        console.log(response.data)
+        response.data.forEach(item => {
+          console.log(item[0])
+          let ary = JSON.parse(item[0])
+          console.log(ary)
+          vm.messageList.push(ary)
         })
-        vm.$emit('load', false)
+        // let ary = JSON.parse(response.data)
+        // vm.messageList.push(ary)
+      })
+    },
+    addMessage () {
+      // msgapi
+      const vm = this
+      const timestamp = new Date()
+      let yyyy = timestamp.getFullYear()
+      let mm = timestamp.getMonth() + 1
+      let day = timestamp.getDate()
+      let hour = timestamp.getHours()
+      let min = timestamp.getMinutes()
+      let sec = timestamp.getSeconds()
+      let time = `${yyyy} / ${mm} / ${day}  ${hour}:${min}:${sec}`
+      // var data = {
+      //   'author': vm.newMessage.author,
+      //   'male': vm.newMessage.male,
+      //   'content': vm.newMessage.content,
+      //   'time': time,
+      //   'good': 0
+      // }
+
+      console.log(typeof vm.newMessage.author)
+      // var aa = '席進便'
+      // var mm = 'boy'
+      // var cc = '哈哈哈哈哈'
+      // var tt = '2022 / 4 / 25  22:29:36'
+      var gg = 0
+      var data = {
+        'author': vm.newMessage.author,
+        'male': vm.newMessage.male,
+        'content': vm.newMessage.content,
+        'time': time,
+        'good': gg
+      }
+      // data = JSON.stringify(data)
+      // 可用ajax
+      $.ajax({
+        // 這邊用get type
+        type: 'get',
+        // api url - google appscript 產出的 url
+        url: 'https://script.google.com/macros/s/AKfycbySNI7cijbkYD9Wzw3Qb22XUHwHkRj8c7utSYAA6NAmGADK-wL1nCAyoo6VA_cfBQD4/exec',
+        // 剛剛整理好的資料帶入
+        data: data,
+        // 資料格式是JSON
+        dataType: 'JSON',
+        // 成功送出 會回頭觸發下面這塊感謝
+        success: function (response) {
+          console.log(response)
+          alert('恭喜成功送出，已具備抽獎資格')
+        },
+        error: function (response) {
+          console.log(response)
+          alert('系統出錯，請稍後在試一次或者聯絡客服人員')
+        }
       })
     }
-    // addMessage () {
-    //   // msgapi
-    //   const vm = this
-    //   const timestamp = new Date()
-    //   let yyyy = timestamp.getFullYear()
-    //   let mm = timestamp.getMonth() + 1
-    //   let day = timestamp.getDate()
-    //   let hour = timestamp.getHours()
-    //   let min = timestamp.getMinutes()
-    //   let sec = timestamp.getSeconds()
-    //   let time = {
-    //     time: `${yyyy} / ${mm} / ${day}  ${hour}:${min}:${sec}`
-    //   }
-    //   var data = Object.assign({}, vm.newMessage, time)
-    //   console.log(data)
-    //   // data = JSON.stringify(data)
-    //   // console.log(data)
-    //   // 可用ajax
-    //   $.ajax({
-    //     // 這邊用get type
-    //     type: 'get',
-    //     // api url - google appscript 產出的 url
-    //     url: 'https://script.google.com/macros/s/AKfycbwrLoDh54vq48S5kbwCXSsx3Qq0ev4yH1Q3LH65yCsRoqSRojqqlqxHgGNTN-vOR5tb/exec',
-    //     // 剛剛整理好的資料帶入
-    //     data: data,
-    //     // 資料格式是JSON
-    //     dataType: 'JSON',
-    //     // 成功送出 會回頭觸發下面這塊感謝
-    //     success: function (response) {
-    //       console.log(response)
-    //       alert('恭喜成功送出，已具備抽獎資格')
-    //     },
-    //     error: function (response) {
-    //       console.log(response)
-    //       alert('系統出錯，請稍後在試一次或者聯絡客服人員')
-    //     }
-    //   })
-    // }
   },
   computed: {
     submit () {
